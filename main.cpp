880b8b58a0e049efa515a54d14eccbc467c2ddf2
2sanghwa <55964051+2sanghwa@users.noreply.github.com>
2019-12-10 15:01:31 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@ int main()
     {
         switch (manager.GetStatus())
         {
-        case GAMESTATE::GAMEINIT:
+            case GAMESTATE::INIT:
             manager.InitGame();
             break;
         case GAMESTATE::READY:
             manager.MainMenu();
             break;
         case GAMESTATE::LEVELSELECTION:
+            manager.LevelSelcetion();
             break;
         case GAMESTATE::OPENCARD:
             manager.OpenCard();
@@ -22,12 +23,16 @@ int main()
             manager.PlayGame();
             break;
         case GAMESTATE::SUCCESS:
+            manager.GameSuccess();
             break;
         case GAMESTATE::FAIL:
+            manager.GameOver();
             break;
         case GAMESTATE::RESULT:
+            manager.Result();
             break;
         case GAMESTATE::CLOSEGAME:
+            manager.EndGame();
             return 0;
         default:
             manager.DefaultException();
